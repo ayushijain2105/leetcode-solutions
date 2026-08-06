52a329cc30095a06bf3e56d84c39223b05613cdc
AYUSHI JAIN <AYUSHIJAINJ095@GMAIL.COM>
2026-08-07 01:57:50 +0530

    Time: 77 ms (12.69%), Space: 48.1 MB (13.33%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0313-super-ugly-number/0313-super-ugly-number.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        PriorityQueue<int[]> queue=new PriorityQueue<>((a,b)->(a[0]-b[0]));
+        for (int i=0;i<primes.length;i++)
+            queue.offer(new int[]{primes[i], primes[i], 0});
+
+        int[] nums=new int[n+1];
+        nums[0]=1;
+        
+        int i=1;
+        while (i<n){
+            int[] entry=queue.poll();
+            int num=entry[0], prime=entry[1], index=entry[2];
+			// remove duplicate
+            if (num!=nums[i-1]){
+                nums[i]=num;
+                i++;
+            }
+            queue.offer(new int[]{prime*nums[index+1], prime, index+1});
+        }
+        return nums[n-1];
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
