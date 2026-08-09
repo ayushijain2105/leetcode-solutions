349e18c4c12592d1cf9ccb615330596625ba32ef
AYUSHI JAIN <AYUSHIJAINJ095@GMAIL.COM>
2026-08-09 23:10:07 +0530

    Time: 0 ms (100%), Space: 42 MB (71.65%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        if(n%2==0) return true;
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
