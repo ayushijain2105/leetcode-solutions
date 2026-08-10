aa04cefdd2cbb114d402838dfad8264a9c658219
AYUSHI JAIN <AYUSHIJAINJ095@GMAIL.COM>
2026-08-11 00:01:38 +0530

    Time: 0 ms (100%), Space: 42.2 MB (35.25%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int tribonacci(int n) {
+        Map<Integer, Integer> memo = new HashMap<>();
+        return helper(n, memo);
+    }
+    
+    private int helper(int n, Map<Integer, Integer> memo) {
+        if (memo.containsKey(n)) {
+            return memo.get(n);
+        }
+        
+        int result;
+        if (n == 0) {
+            result = 0;
+        } else if (n == 1 || n == 2) {
+            result = 1;
+        } else {
+            result = helper(n - 1, memo) + helper(n - 2, memo) + helper(n - 3, memo);
+        }
+        
+        memo.put(n, result);
+        return result;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
